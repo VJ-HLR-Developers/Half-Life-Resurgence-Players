e2ee5c7c23055d70bd63958e09ca9b820dae4b0e
Mawskeeto1223 <tomaxx11@gmail.com>
2022-04-28 20:56:24 +0300

    corrected debris sound level for robo

--- lua/entities/npc_vj_hlr1mp_robo/init.lua
+++ lua/entities/npc_vj_hlr1mp_robo/init.lua
@@ -79,7 +79,7 @@ function ENT:CustomOnDeath_AfterCorpseSpawned(dmginfo, hitgroup, corpseEnt)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomGibOnDeathSounds(dmginfo, hitgroup)
-	VJ_EmitSound(self, "vj_hlr/hl1_weapon/explosion/debris3.wav", 150, 100)
+	VJ_EmitSound(self, "vj_hlr/hl1_weapon/explosion/debris3.wav", 80, 100)
 	VJ_EmitSound(self, "vj_hlr/hl1_npc/rgrunt/rb_gib.wav", 80, 100)
 	return false
 end
