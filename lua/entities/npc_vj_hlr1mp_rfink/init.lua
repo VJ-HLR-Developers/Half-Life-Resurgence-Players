98cc6b7273b69a48597549370bee99278382e255
Mawskeeto1223 <tomaxx11@gmail.com>
2023-01-19 04:40:31 +0200

    Rat Fink (RFink) is now real

---
+++ lua/entities/npc_vj_hlr1mp_rfink/init.lua
@@ -0,0 +1,10 @@
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+/*-----------------------------------------------
+	*** Copyright (c) 2012-2023 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------*/
+ENT.Model = {"models/vj_hlr/hl1mp/rfink.mdl"}
+ENT.VJ_NPC_Class = {"CLASS_MP_RFINK"}
+---------------------------------------------------------------------------------------------------------------------------------------------
