55210926d0d1f71c5e516aced42a3edaeef4573d
Darkborn <darkandrai1987@gmail.com>
2026-06-11 01:21:26 -0400

    Update init.lua

--- lua/entities/npc_vj_hlr1_freeman/init.lua
+++ lua/entities/npc_vj_hlr1_freeman/init.lua
@@ -332,7 +332,7 @@ local transDeath = {
 local defDeath = {ACT_DIESIMPLE, ACT_DIEFORWARD, ACT_DIEBACKWARD}
 --
 function ENT:OnDeath(dmginfo, hitgroup, status)
-	if status == "Init" && GetConVar("vj_hlr1_corpse_static"):GetInt() == 1 && VJ_CVAR_AI_ENABLED then
+	if status == "Init" && GetConVar("vj_hlr1_corpse_static"):GetInt() == 1 && VJ_CVAR_AI_ENABLED && self.HasDeathAnimation then
 		self.DeathAnimationDecreaseLengthAmount = -1
 		self.DeathCorpseEntityClass = "prop_vj_animatable"
 	elseif status == "DeathAnim" then
