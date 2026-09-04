5587e06e688eb3fb62a84d57cfcb6755fe4a8095
Darkborn <darkandrai1987@gmail.com>
2026-09-04 19:17:35 -0400

    Fix Mike not using DM base

--- lua/entities/npc_vj_hlr1mp_mike/init.lua
+++ lua/entities/npc_vj_hlr1mp_mike/init.lua
@@ -1,4 +1,4 @@
-include("entities/npc_vj_hlr1_freeman/init.lua")
+include("entities/npc_vj_hlr1mp_gordon/init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 /*-----------------------------------------------
